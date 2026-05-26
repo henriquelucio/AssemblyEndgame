63e0832ba42d42179c2c479d2bc3d77d034681f3
Henrique <henriquelucio95@hotmail.com>
2026-05-26 19:45:51 -0300

    Lesson 8

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,51 @@
 import React from "react"
+import {languages} from "./languages"
 
-export default function(){
+export default function AssemblyEndgame(){
+    const [currentWord, setCurrentWord] = React.useState("react")
+
+    const alphabet = "abcdefghijklmnopqrstuvwyxz"
+    
+    const languagesElement = languages.map(lang => {
+        const styles = {
+            backgroundColor: lang.backgroundColor,
+            color: lang.color
+        }
+        return (
+        <span 
+            className="chip" 
+            style={styles} 
+            key={lang.name}
+        >{lang.name}</span>
+    )})
+
+    const letterElements = currentWord.split("").map((letter, index) => (
+        <span key={index}>{letter.toUpperCase()}</span>
+    ))
+
+    const keyboardElements = alphabet.split("").map((letter) => (
+        <button key={letter}>{letter.toUpperCase()}</button>
+    ))
+    
     return(
-        <h1>Teste</h1>
+        <main>
+            <header>
+                <h1>Assembly Endgame</h1>
+                <p>Guess the word within 8 attempts to keep the programming world safe from Assembly!</p>
+            </header>
+            <section className="game-status">
+                <h2>You won!</h2>
+                <p>Hit new game to play again</p>
+            </section>
+            <section className="language-chips">
+                {languagesElement}
+            </section>
+            <section className="word">
+                {letterElements}
+            </section>
+            <section className="keyboard">
+                {keyboardElements}
+            </section>
+        </main>
     )
 }
